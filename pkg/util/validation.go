ee4d2ffc33e2b1914a4260c28946359c5d09d4b3
jackyes <jackyes@users.noreply.github.com>
2024-10-30 13:00:34 +0100

    Add files via upload

--- pkg/util/validation.go
+++ pkg/util/validation.go
@@ -2,13 +2,12 @@ package util
 
 import (
 	"net/http"
-	"regexp"
 	"strings"
 )
 
 var (
 	// Regex to validate the path
-	validPathRegex = regexp.MustCompile(`^[a-zA-Z0-9\-\_\/\.\?\=\&\%\+\@\(\)]*$`)
+	//validPathRegex = regexp.MustCompile(`^[a-zA-Z0-9\-\_\/\.\?\=\&\%\+\@\(\)]*$`)
 	// Metodi HTTP consentiti
 	validMethods = map[string]bool{
 		http.MethodGet:     true,
@@ -21,22 +20,23 @@ var (
 	}
 )
 
-	// ValidatePath checks if the path is secure
+// ValidatePath checks if the path is secure
 func ValidatePath(path string) bool {
 	// Prevents path traversal and ensures the path starts with a slash
 	if strings.Contains(path, "..") || !strings.HasPrefix(path, "/") {
 		return false
 	}
 	// Checks that it contains only allowed characters
-	return validPathRegex.MatchString(path)
+	//return validPathRegex.MatchString(path)
+	return true
 }
 
-	// ValidateMethod checks if the HTTP method is allowed
+// ValidateMethod checks if the HTTP method is allowed
 func ValidateMethod(method string) bool {
 	return validMethods[strings.ToUpper(method)]
 }
 
-	// ValidateHost checks if the host is valid
+// ValidateHost checks if the host is valid
 func ValidateHost(host string) bool {
 	// Prevents header injection and ensures the host is not empty
 	return host != "" && !strings.ContainsAny(host, "\r\n")
