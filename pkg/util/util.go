facf12e5457ccfc60d63b6ef1963bce2e75f4acc
jackyes <jackyes@users.noreply.github.com>
2024-11-22 09:30:05 +0100

    Update util.go

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -3,12 +3,21 @@ package util
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
-	"github.com/jackyes/underpass/pkg/models"
 	"github.com/gorilla/websocket"
+	"github.com/jackyes/underpass/pkg/models"
 	"github.com/vmihailenco/msgpack/v5"
 )
 
+// msgPackPool is a sync.Pool for reusing byte slices to reduce memory allocations.
+var msgPackPool = &sync.Pool{
+	New: func() interface{} {
+		return make([]byte, 0, 1024) // Initial capacity
+	},
+}
+
+// MarshalRequest validates the HTTP request and converts it into a models.Request object.
 func MarshalRequest(r *http.Request) (models.Request, error) {
 	// Validates fields before creating the Request
 	if !ValidatePath(r.RequestURI) {
@@ -29,6 +38,7 @@ func MarshalRequest(r *http.Request) (models.Request, error) {
 	}, nil
 }
 
+// MarshalResponse converts an HTTP response into a models.Response object.
 func MarshalResponse(r *http.Response) models.Response {
 	return models.Response{
 		Headers:    r.Header,
@@ -36,21 +46,29 @@ func MarshalResponse(r *http.Response) models.Response {
 	}
 }
 
-	// Writes a MessagePack-encoded binary string to a WebSocket connection
+// WriteMsgPack marshals the given data into MessagePack format and writes it to the WebSocket connection.
 func WriteMsgPack(c *websocket.Conn, v interface{}) error {
+	b := msgPackPool.Get().([]byte)
+	defer msgPackPool.Put(b)
+
+	b = b[:0] // Reset the slice
+
 	marshalled, err := msgpack.Marshal(v)
 	if err != nil {
-		return fmt.Errorf("failed to write message: %w", err)
+		return fmt.Errorf("failed to marshal data: %w", err)
 	}
 
-	err = c.WriteMessage(websocket.BinaryMessage, marshalled)
+	b = append(b, marshalled...)
+
+	err = c.WriteMessage(websocket.BinaryMessage, b)
 	if err != nil {
-		return fmt.Errorf("failed to read message: %w", err)
+		return fmt.Errorf("failed to write message: %w", err)
 	}
 
 	return nil
 }
 
+// ReadMsgPack reads a MessagePack formatted message from the WebSocket connection and unmarshals it into the given interface.
 func ReadMsgPack(c *websocket.Conn, v interface{}) error {
 	_, m, err := c.ReadMessage()
 	if err != nil {
